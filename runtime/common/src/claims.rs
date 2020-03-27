2d29e9de8285c4fd5536b133f2e355b20a322ece
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-27 23:15:48 +0100

    add verify

--- runtime/common/src/claims.rs
+++ runtime/common/src/claims.rs
@@ -646,6 +646,8 @@ mod benchmarking {
 	use sp_runtime::DispatchResult;
 	use sp_runtime::traits::ValidateUnsigned;
 	use crate::claims::Call;
+	use frame_support::ensure;
+	use frame_support::storage::IterableStorageMap;
 
 	const SEED: u32 = 0;
 
@@ -719,6 +721,30 @@ mod benchmarking {
 			for _ in 0 .. i {
 				let _maybe_signer = super::Module::<T>::eth_recover(&signature, &data);
 			}
+		}
+
+		verify_10 {
+			let i in 0 .. 10;
+		}: {
+			ensure!(system::Account::<T>::iter().count() >= 10, "less than 10 users");
+		}
+
+		verify_100 {
+			let i in 0 .. 10;
+		}: {
+			ensure!(system::Account::<T>::iter().count() >= 100, "less than 100 users");
+		}
+
+		verify_1000 {
+			let i in 0 .. 10;
+		}: {
+			ensure!(system::Account::<T>::iter().count() >= 1000, "less than 1000 users");
+		}
+
+		verify_10000 {
+			let i in 0 .. 10;
+		}: {
+			ensure!(system::Account::<T>::iter().count() >= 10000, "less than 10000 users");
 		}
 	}
 }
